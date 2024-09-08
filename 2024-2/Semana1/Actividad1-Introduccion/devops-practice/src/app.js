ae2b75fbe47d616eb46d3f4acca3225cfda37d7e
kapumota <121532729+kapumota@users.noreply.github.com>
2024-09-08 06:17:48 -0500

    Update app.js

--- 2024-2/Semana1/Actividad1-Introduccion/devops-practice/src/app.js
+++ 2024-2/Semana1/Actividad1-Introduccion/devops-practice/src/app.js
@@ -67,4 +67,69 @@ if (require.main === module) {
     });
 }
 
+/*usa este cambio para usar prometheus(modifica tu app.js original por este archivo)
+const express = require('express');
+const app = express();
+const client = require('prom-client');
 
+// Configurar la colección de métricas por defecto para Prometheus
+client.collectDefaultMetrics();
+
+const requestCounter = new client.Counter({
+  name: 'node_request_operations_total',
+  help: 'Total number of requests',
+  labelNames: ['method', 'route', 'status_code']
+});
+
+const responseHistogram = new client.Histogram({
+  name: 'node_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status_code']
+});
+
+// Middleware para contar las solicitudes y medir la duración
+app.use((req, res, next) => {
+  const end = responseHistogram.startTimer({
+    method: req.method,
+    route: req.path,
+  });
+
+  res.on('finish', () => {
+    requestCounter.inc({
+      method: req.method,
+      route: req.path,
+      status_code: res.statusCode
+    });
+
+    end({ status_code: res.statusCode });
+  });
+
+  next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/delay', (req, res) => {
+    setTimeout(() => {
+        res.send('This was delayed by 2 seconds');
+    }, 2000);
+});
+
+// Endpoint para métricas de Prometheus
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+});
+
+module.exports = app;
+
+if (require.main === module) {
+    const port = process.env.PORT || 3001;  // Asegurar que el puerto es estático
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
+
+*/
